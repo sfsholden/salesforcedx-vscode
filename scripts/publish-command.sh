125e6235c3941ab80365947176884af8dc58d0bb
Shelby Holden <shelby.holden@salesforce.com>
2020-07-07 17:48:38 -0600

    fixed script

--- scripts/publish-command.sh
+++ scripts/publish-command.sh
@@ -1,8 +1,7 @@
 curl -v -u ${CircleCIToken}: -X POST --header "Content-Type: application/json" -d '{
   "branch": "master",
   "parameters": {
-    "deploy": true,
-    "version": "patch"
+    "publish": true
   }
 }' https://circleci.com/api/v2/project/gh/sfsholden/salesforcedx-vscode-sholden
 
