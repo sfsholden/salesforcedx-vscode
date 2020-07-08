7a000f4da9ea0a8c0fe9b7d4bcdf8aed4432d214
Shelby Holden <shelby.holden@salesforce.com>
2020-07-07 18:38:04 -0600

    Changed to dev

--- scripts/publish-command.sh
+++ scripts/publish-command.sh
@@ -4,4 +4,4 @@ curl -v -u ${CircleCIToken}: -X POST --header "Content-Type: application/json" -
   "parameters": {
     "publish": true
   }
-}' https://circleci.com/api/v2/project/gh/sfsholden/salesforcedx-vscode-sholden
+}' https://circleci.com/api/v2/project/gh/sfsholden/salesforcedx-vscode-sholden/pipeline
