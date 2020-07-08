5d1bdce67971c8f0d7387975976e3fd4181d1e33
Shelby Holden <shelby.holden@salesforce.com>
2020-07-07 18:38:04 -0600

    Changed to dev

--- scripts/create-release-branch-command.sh
+++ scripts/create-release-branch-command.sh
@@ -2,6 +2,6 @@ CircleCIToken=$1
 curl -v -u ${CircleCIToken}: -X POST --header "Content-Type: application/json" -d '{
   "branch": "develop",
   "parameters": {
-    "create-release-branch": true
+    "create-release": true
   }
-}' https://circleci.com/api/v2/project/gh/sfsholden/salesforcedx-vscode-sholden
+}' https://circleci.com/api/v2/project/gh/sfsholden/salesforcedx-vscode-sholden/pipeline
