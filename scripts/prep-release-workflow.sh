b80e9c4c981996d9d2032fbde8042577685e7775
Shelby <shelby.holden@salesforce.com>
2020-09-28 16:15:07 -0600

    chore: fix release branch

--- scripts/prep-release-workflow.sh
+++ scripts/prep-release-workflow.sh
@@ -2,8 +2,11 @@
 CircleCIToken=$1
 ReleaseBranch=$2
 curl -v -u ${CircleCIToken}: -X POST --header "Content-Type: application/json" -d '{
-  "branch": "origin/release/v'${ReleaseBranch}'",
+  "branch": "release/v'${ReleaseBranch}'",
   "parameters": {
     "prep-release": true
   }
 }' https://circleci.com/api/v2/project/gh/sfsholden/salesforcedx-vscode-sholden/pipeline
+
+# open the release pipe line url
+open "https://app.circleci.com/pipelines/github/sfsholden/salesforcedx-vscode-sholden"
